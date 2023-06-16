64a64c6192c26f6f5441caeffaf0188a9dbad105
Andrei Avramenko <aladser@gmail.com>
2023-06-16 18:26:16 +0900

    кнопка отправки сообщения

--- application/views/chats_view.php
+++ application/views/chats_view.php
@@ -1,34 +1,45 @@
-<div class='container chats h-80 bg-C4C4C4 p-0 d-flex'>
-    <div class='contacts pt-2 ps-2 pe-2'>
-        <div class='split-word'>JohnJohnJohnJohnJohn</div>
-        <div class='split-word'>MaryMaryMaryMaryMary</div>
-        <div class='split-word'>PeterPeterPeterPeterPeter</div>
+<div class='container font-roboto h-80 bg-C4C4C4 p-0 d-flex'>
+    <div class='contacts-container pt-2 ps-2 pe-2'>
+        <div class='split-word'> <img class='pe-2 pb-2' src="application/images/ava.png" alt="фото"><span>Andrei Avramenko</span> </div>
+        <div class='split-word'><img class='pe-2 pb-2' src="application/images/ava.png" alt="фото"><span>Denis Demin</span></div>
+        <div class='split-word'><img class='pe-2 pb-2' src="application/images/ava.png" alt="фото"><span>Евгения Городецкая</span></div>
+        <div class='split-word'><img class='pe-2 pb-2' src="application/images/ava.png" alt="фото"><span>Богдан Барабанщиков</span></div>
     </div>
-    <div class='messages pt-2 ps-4 pe-4'>
-        <div>Чат с пользователем aladser@mail.ru</div>
+    <div class='messages-container pt-2 ps-4 pe-2 border-start border-end border-dark position-relative'>
+        <p class='pb-2'>Чат с пользователем aladser@mail.ru</p>
+
         <div>
-            <div class='border'>
+            <div class=''>
                 Штиль – ветер молчит
                 Упал белой чайкой на дно
                 Штиль – наш корабль забыт
                 Один, в мире скованном сном...
             </div>
-            <div class='border'>
+            <div class=''>
                 Между всех времён
                 Без имён и лиц
                 Мы уже не ждём,
                 Что проснётся бриз!
             </div>
-            <div class='border'>
+            <div class=''>
                 Что нас ждёт, море хранит молчанье
                 Жажда жить сушит сердца до дна
                 Только жизнь здесь ничего не стоит
                 Жизнь других, но не твоя!
             </div>
         </div>
-        <div>Отправите сообщение</div>
+
+        <div class='position-absolute bottom-0 mb-2 w-100'>
+            <form action="">
+                <div class='d-flex'>
+                    <textarea class="send-msg__text ps-2 w-90 resize-none border"rows='2' placeholder='Наберите сообщение здесь'></textarea>
+                    <input type="image" src='application/images/sendbtn.png' class="border">
+                </div>
+            </form>
+        </div>
+
     </div>
-    <div class='options pt-2'>
+    <div class='options-container pt-2'>
         <div class='option'><a href="" class='option-href'>Профиль</a></div>
         <div class='option'><a href="" class='option-href'>Настройки</a></div>
         <div class='option'><a href="" class='option-href'>Выйти</a></div>
