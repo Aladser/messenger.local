cecc0d7513452872702407e9f4555b2113f0cfa7
Andrei Avramenko <aladser@gmail.com>
2023-06-19 10:30:22 +0900

    верстка чатов

--- application/views/chats_view.php
+++ application/views/chats_view.php
@@ -8,31 +8,40 @@
     <div class='messages-container pt-2 ps-4 pe-2 border-start border-end border-dark position-relative'>
         <p class='pb-2'>Чат с пользователем aladser@mail.ru</p>
 
-        <div>
-            <div class=''>
+        <div class='messages'>
+            <div class='messages__msg messages__owner-msg'>
                 Штиль – ветер молчит
                 Упал белой чайкой на дно
                 Штиль – наш корабль забыт
                 Один, в мире скованном сном...
+                <p class='text-rgb66666'>20.06.203 10.30</p>
             </div>
-            <div class=''>
+
+            <div class='messages__msg'>
                 Между всех времён
                 Без имён и лиц
                 Мы уже не ждём,
                 Что проснётся бриз!
+                <p class='text-rgb66666'>20.06.203 10.40</p>
             </div>
-            <div class=''>
+            <div class='messages__msg messages__owner-msg'>
                 Что нас ждёт, море хранит молчанье
                 Жажда жить сушит сердца до дна
                 Только жизнь здесь ничего не стоит
                 Жизнь других, но не твоя!
+                <p class='text-rgb66666'>20.06.203 10.50</p>
+            </div>
+             <div class='messages__msg'>
+                Его кровь и плоть вновь насытит нас.
+                И за смерть ему может Бог воздасть
+                <p class='text-rgb66666'>20.06.203 10.50</p>
             </div>
         </div>
 
         <div class='position-absolute bottom-0 mb-2 w-100'>
             <form action="">
                 <div class='d-flex'>
-                    <textarea class="send-msg__text ps-2 w-90 resize-none border"rows='2' placeholder='Наберите сообщение здесь'></textarea>
+                    <textarea class="ps-2 w-90 resize-none border"rows='2' placeholder='Наберите сообщение здесь'></textarea>
                     <input type="image" src='application/images/sendbtn.png' class="border">
                 </div>
             </form>
