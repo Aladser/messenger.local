8a3703bbb5a352ba09a44598074a374854851ee0
“Aladser” <aladser@gmail.com>
2023-08-04 19:05:26 +0900

    запуск вебсокета в Linux

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace Aladser;
-
 use Aladser\Core\Route;
-
 require __DIR__ . '/vendor/autoload.php';
-
 date_default_timezone_set('Europe/Moscow');
-
 Route::start();
+
+//**** поиск запущенного демона вебсокета ****
+$pidFile = __DIR__.'/pids.txt';
+$fd = fopen($pidFile, 'w');
+fwrite($fd, ''); // чистка файла от старых данных
+exec("ps aux | grep chat-server > $pidFile"); // новая таблица pidов
+$isWebSocket = count(file($pidFile)) > 2; // чтение новой таблицы pidов
+fclose($fd);
