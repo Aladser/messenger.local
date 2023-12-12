24366b949beb385933208d9d237627b01f50f672
Aladser <aladser@gmail.com>
2023-12-12 16:32:39 +0900

    добавлены комментарии

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -8,6 +8,7 @@ use Aladser\Core\ScriptLinuxProcess;
 require_once __DIR__.'/vendor/autoload.php';
 date_default_timezone_set('Europe/Moscow');
 
+// php /var/www/messenger.local/application/chat-server.php > /var/www/messenger.local/logs/websocket.log
 $os = explode(' ', php_uname())[0];
 if ($os !== 'Windows') {
     $websocket = new ScriptLinuxProcess(
