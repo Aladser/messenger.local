00e2abca248b8911dd4203f987c82bb58740f371
Aladser <aladser@gmail.com>
2023-12-29 14:13:58 +0900

    обновленеи структуры проекта

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -10,17 +10,17 @@
 
     <title><?php echo $page_name; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="icon" href="http://<?php echo $app_name; ?>/application/images/icon.png">
-    <link rel="stylesheet" href="http://<?php echo $app_name; ?>/application/css/reset_styles.css">
-    <link rel="stylesheet" href="http://<?php echo $app_name; ?>/application/css/template.css">
+    <link rel="icon" href="http://<?php echo $app_name; ?>/public/images/icon.png">
+    <link rel="stylesheet" href="http://<?php echo $app_name; ?>/public/css/reset_styles.css">
+    <link rel="stylesheet" href="http://<?php echo $app_name; ?>/public/css/template.css">
     <?php if (!empty($content_css)) { ?>
-        <link rel="stylesheet" href="http://<?php echo $app_name; ?>/application/css/<?php echo $content_css; ?>">
+        <link rel="stylesheet" href="http://<?php echo $app_name; ?>/public/css/<?php echo $content_css; ?>">
     <?php } ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- js скрипты -->
     <?php if (!empty($js_script_list)) { ?>
         <?php foreach ($js_script_list as $script) { ?>
-            <script type='text/javascript' src="http://<?php echo $app_name; ?>/application/js/<?php echo $script; ?>" defer></script>
+            <script type='text/javascript' src="http://<?php echo $app_name; ?>/public/js/<?php echo $script; ?>" defer></script>
         <?php } ?>
     <?php } ?>
 </head>
