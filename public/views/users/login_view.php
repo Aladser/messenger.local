7fb20860389e280f4199ce11f0d6b24cf1a08d94
Aladser <aladser@gmail.com>
2024-01-16 19:50:59 +0900

    обновление цветовой схемы

--- public/views/users/login_view.php
+++ public/views/users/login_view.php
@@ -3,20 +3,20 @@
         <input type="hidden" name="CSRF" value=<?php echo $data['csrf']; ?>>
         <input type="hidden" name="login">
 
-        <label for="login-form__email-input" class='text-start btn-width p-1 fw-bolder'>Почта</label>
-        <input type="email" class="d-block mx-auto btn-width mb-2 p-2 border-color-theme" 
+        <label for="login-form__email-input" class='text-start theme-width-30 p-1 fw-bolder'>Почта</label>
+        <input type="email" class="d-block mx-auto theme-width-30 mb-2 p-2 border-color-theme" 
             id="login-form__email-input" name='email' placeholder='Почта' 
             required>
 
-        <label for="login-form__password-input" class='text-start btn-width p-1 fw-bolder'>Пароль</label>
-        <input type="password" class="d-block mx-auto btn-width mb-3 p-2 border-color-theme" 
+        <label for="login-form__password-input" class='text-start theme-width-30 p-1 fw-bolder'>Пароль</label>
+        <input type="password" class="d-block mx-auto theme-width-30 mb-4 p-2 border-color-theme" 
             id="login-form__password-input" name='password' 
             placeholder='Пароль' 
             required>
         
-        <input type="submit" class='d-block mx-auto btn-width btn mb-2 btn-bg-theme text-white p-3 rounded' 
+        <input type="submit" class='d-block mx-auto theme-width-30 btn mb-2 btn-bg-theme text-white p-3 rounded' 
             value="Войти" id='login-form__login-btn'>
-        <a href="/"><p class='d-block btn-width mx-auto btn-bg-theme text-white p-3 rounded'>Назад</p></a>
+        <a href="/"><p class='d-block theme-width-30 mx-auto btn-bg-theme text-white p-3 rounded'>Назад</p></a>
     </form>
     <?php if (isset($data['error'])) { ?>
         <p class='w-50 mx-auto fw-bolder text-danger pt-2 mb-0' id='login-error'><?php echo $data['error']; ?></p>
--- public/views/users/register_view.php
+++ public/views/users/register_view.php
@@ -3,26 +3,27 @@
         <input type="hidden" name ='CSRF' id="input-csrf" value=<?php echo $data['csrf']; ?>>
         <input type="hidden" name="registration">
 
-        <label for="login-form__email-input" class='text-start btn-width p-1 fw-bolder'>Почта</label>
-        <input type="email" class="d-block btn-width mx-auto mb-2 p-2 border-color-theme" 
+        <label for="login-form__email-input" class='text-start theme-width-30 p-1 fw-bolder'>Почта</label>
+        <input type="email" class="d-block theme-width-30 mx-auto mb-2 p-2 border-color-theme" 
             id="reg-form__email-input" name='email' 
             placeholder='адрес электронной почты' 
             required>
 
-        <label for="login-form__email-input" class='text-start btn-width p-1 fw-bolder'>Пароль</label>
-        <input type="password" class="d-block btn-width mx-auto mb-2 p-2 border-color-theme" 
+        <label for="login-form__email-input" class='text-start theme-width-30 p-1 fw-bolder'>Пароль</label>
+        <input type="password" class="d-block theme-width-30 mx-auto mb-2 p-2 border-color-theme" 
             id="reg-form__password1-input" name='password' 
             placeholder="пароль: минимум 8 символов (буквы и цифры)" 
             required>
-        <input type="password" class="d-block btn-width mx-auto mb-2 p-2 border-color-theme" 
+        <label for="login-form__email-input" class='text-start theme-width-30 p-1 fw-bolder'>Подтвердите пароль</label>
+        <input type="password" class="d-block theme-width-30 mx-auto mb-4 p-2 border-color-theme" 
             id="reg-form__password2-input" name='password_confirm' 
             placeholder='подтвердите пароль' 
             required>
         
-        <input type="submit" class='d-block btn-width btn mx-auto mb-2 btn-bg-theme text-white p-3 rounded' 
+        <input type="submit" class='d-block theme-width-30 btn mx-auto mb-2 btn-bg-theme text-white p-3 rounded' 
             value="Регистрация" id='reg-form__reg-btn'>
         <a href="/" class='text-decoration-none'>
-            <p class='d-block btn-width mx-auto btn-bg-theme text-white p-3 rounded'>Назад</p>
+            <p class='d-block theme-width-30 mx-auto btn-bg-theme text-white p-3 rounded'>Назад</p>
         </a>
         
         <?php if (isset($data['error'])) { ?>
